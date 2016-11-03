bd54ed4ba31468d0d5b5b977dade86ba718b05ef
orggue <jorgechato1@gmail.com>
2016-11-03 22:43:49 +0100

    main en raw

--- practica1.cpp
+++ practica1.cpp
@@ -31,9 +31,6 @@ int main(){
   crearInstante(i,1970,1,1,00,00,00);
   crear("MOCOSOFT",i,10.0,e);
 
-  bool error;
-  abrirSesion(e,error);
-
   ifstream file("entrada.txt");
   ofstream salida;
   salida.open("salida.txt");
@@ -61,29 +58,69 @@ int main(){
           atoi(hora.substr(0, ph).c_str()),
           atoi(hora.substr(ph+1, sh-ph-1).c_str()),
           atoi(hora.substr(sh+1).c_str()));
+
       bool introducido = introducir(e, i2, atof(accion.c_str()));
       string instante;
       generaInstanteCadena(i2, instante);
 
       if(introducido){
-        salida << "INSERCION: " + instante + ";" + identificador(e) + "\n";
+        salida << "INSERCION: " << instante << ";" << identificador(e) << "\n";
       }else{
-        salida << "INSERCION DESECHADA: " + instante + ";" + identificador(e) + "\n";
+        salida << "INSERCION DESECHADA: " << instante << ";" << identificador(e) << "\n";
       }
     }
     if(content == "ue"){
+      instante iMenor = ultimoInstante(e);
+      float valorMenor = ultimoValor(e);
+      string instante;
+      generaInstanteCadena(iMenor, instante);
+
+        salida << "ULTIMO: " << instante << ";" << valorMenor << "\n";
     }
     if(content == "me"){
+      instante iMayor;
+      float valorMayor;
+      string instante;
+      instanteMaximoMayorValor(e, iMayor, valorMayor);
+      generaInstanteCadena(iMayor, instante);
+
+        salida << "MAYOR: " << instante << ";" << valorMayor << "\n";
     }
     if(content == "ae"){
+      bool error;
+      abrirSesion(e, error);
+
+      if(error){
+        salida << "APERTURA DE SESION DESECHADA" << "\n";
+      }else{
+        salida << "SESION ABIERTA" << "\n";
+      }
     }
     if(content == "ce"){
+      bool error;
+      cerrarSesion(e, error);
+
+      if(error){
+        salida << "CIERRE DE SESION DESECHADO" << "\n";
+      }else{
+        salida << "SESION CERRADA" << "\n";
+      }
     }
     if(content == "le"){
+      string datos, historico;
+      listarDatosBaseEmpresa(e, datos);
+      listarHistorico(e, historico);
+
+      if(sesionAbierta(e)){
+        salida << datos << "\n";
+        salida << "TOTAL: " << tamanyoHistorico(e) << "\n";
+        salida << historico;
+      }else{
+        salida << datos << "\n";
+      }
     }
   }
 
-  cerrarSesion(e,error);
   salida.close();
   return 0;
 }
